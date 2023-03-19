ce92284aa6bf199b0b53fbdccc9fc94ad2473bdd
h_k_rony <raindropplus11@gmail.com>
2023-03-19 17:59:58 +0600

    options

---
+++ tests/03-rampup-rampdown.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 5 },
+        { duration: '20s', target: 10 }
+    ]
+
+}
+
+export default function () {
+    http.get('https://www.google.com/')
+}
